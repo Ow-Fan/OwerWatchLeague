42e7961210804e0b8c211747f7906f882eac41df
Ow-Fan <fanjianglong1997@hotmail.com>
2019-09-25 14:36:21 +0800

    springboot+mybatis项目搭建

---
+++ owerwatch/src/main/java/com/fan/owerwatch/OwerwatchApplication.java
@@ -0,0 +1,13 @@
+package com.fan.owerwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwerwatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwerwatchApplication.class, args);
+    }
+
+}
